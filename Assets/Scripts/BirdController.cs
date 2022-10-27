55b1a82d7a44a62f5d61cd42659ebb76de058a0c
Meyra <1meyragonul@gmail.com>
2022-10-27 21:06:19 +0300

    Some functions have been arranged

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -13,30 +13,68 @@ public class BirdController : MonoBehaviour
     public TextMeshProUGUI scoreText;
 
     private GameManager gameManager;
- 
+
+    private float angle;
+    private float minAngle = -10;
+    private float maxAngle = 10;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         gameManager = FindObjectOfType<GameManager>();
     }
 
-    void Update()
+
+    private void Move()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            rb.velocity = Vector2.up * jumpInterval; 
+            rb.velocity = Vector2.up * jumpInterval;
+        }
+    }
+
+
+
+    private void Nod()
+    {
+        if (rb.velocity.y > 0 && angle <= maxAngle)
+        {
+            angle += 4;          
+        }
+        else if (rb.velocity.y < -1.1 && angle > minAngle)
+        {
+            angle -= 2;
         }
 
-        scoreText.text = score.ToString();
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
+
+    void Update()
+    {
+        Move();
+       
+    }
+
+
+    private void FixedUpdate()
+    {
+        Nod();   
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("ScoreManager"))
+        if (gameObject.tag == "Player")
         {
-            score++;
-        }
+            if (collision.gameObject.CompareTag("ScoreManager"))
+            {
+                score++;
+                scoreText.text = score.ToString();
+            }
+        }     
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
--- Assets/Scripts/PipeController.cs
+++ Assets/Scripts/PipeController.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class PipeController : MonoBehaviour
 {
     public float speed;
+    public Transform player;
    
     void Update()
     {
         transform.position += Vector3.left * speed * Time.deltaTime;
+
+        DestroyPipes();
+    }
+
+    private void DestroyPipes()
+    {
+        if (gameObject.transform.position.x < player.position.x)
+        {
+            Debug.Log("Destroy method works");
+            Destroy(gameObject);
+        }
     }
 }
